135d8739284b34fc672b2e26815b422696c0bf0c
Thejus <thejusunni03@gmail.com>
2025-01-18 12:37:35 +0800

    starting server

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,2 +1,31 @@
 package main
 
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thejus03/hacknroll/backend/modules"
+)
+
+//go:embed venues.json
+var venuesJson []byte
+func main() {
+	
+	r := gin.Default()
+	
+	/// Get all coursecodes
+	r.GET("/getAllMods", func(c *gin.Context) { modules.GetAllModules(c)})
+	venueData := make(map[string][]float64)
+
+	/// Post course data and free days
+	r.POST("/getFreeSlots", func (c *gin.Context) {
+		json.Unmarshal(venuesJson, &venueData)
+		modules.Submit(c, venueData)
+	})
+
+	/// Run the server on port 8080
+	r.Run(":8080")
+}
+
