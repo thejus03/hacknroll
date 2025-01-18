084df20e9f2040b829efb723c90dff05b09e41fd
sam <thambad@gmail.com>
2025-01-18 12:27:03 +0800

    pulling the info from  nusmods

--- backend/internal/modules/module.go
+++ backend/internal/modules/module.go
@@ -1,1 +1,23 @@
 package modules
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetAllModules(c *gin.Context) {
+	url := "https://api.nusmods.com/v2/2024-2025/moduleList.json"
+	resp, err := http.Get(url)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	var data []map[string]any
+	json.Unmarshal(body, &data)
+
+}
