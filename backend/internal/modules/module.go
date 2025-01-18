72f3e5dbb641e6a5ef83780466087fae824110de
sam <thambad@gmail.com>
2025-01-18 12:32:16 +0800

    pulling data should work

--- backend/internal/modules/module.go
+++ backend/internal/modules/module.go
@@ -10,6 +10,7 @@ import (
 )
 
 func GetAllModules(c *gin.Context) {
+	//  only returns the list of modules for searching purposes
 	url := "https://api.nusmods.com/v2/2024-2025/moduleList.json"
 	resp, err := http.Get(url)
 	body, err := io.ReadAll(resp.Body)
@@ -19,5 +20,20 @@ func GetAllModules(c *gin.Context) {
 
 	var data []map[string]any
 	json.Unmarshal(body, &data)
+	var courseCode []string
+
+	for _, module := range data {
+		if code, ok := module["moduleCode"].(string); ok {
+			courseCode = append(courseCode, code)
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Error getting module code",
+			})
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Success",
+		"payload": courseCode,
+	})
 
 }
